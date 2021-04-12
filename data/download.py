53f8134c6c813831c1a4dcf4745eb83553b01601
Faisal Hossain <faisal.hossain75@gmail.com>
2021-04-12 16:48:48 -0400

    Added code to download Zillow data

--- data/download.py
+++ data/download.py
@@ -1,7 +1,15 @@
-import requests
-import zipfile
+#!/usr/bin/env python
+# coding: utf-8
+
+# CSCI 4360 - Data Science II Term Project
+# Author: Ayush Kumar, Brandon Amirouche, Faisal Hossain
+
+# Import Libraries
 import os 
+import quandl
+import zipfile
 import sqlite3
+import requests
 import pandas as pd
 
 def download_and_unzip(year): 
@@ -36,9 +44,42 @@ def download_and_unzip(year):
     
     db.close()
 
+def zillow_download(years):
+    """
+    Downloads Zillow Real Estate data from the quandl website for a given year.
+
+    Keyword arguments: 
+    years - list of years of data to be downloaded
+    """
+
+    # Authentication
+    quandl.ApiConfig.api_key = "koRtyVvbCo8Z-rArzJny"
+
+    # API request to get data
+    zillow_data = quandl.get_table('ZILLOW/DATA')
+    zillow_indicators = quandl.get_table('ZILLOW/INDICATORS')
+    zillow_regions = quandl.get_table('ZILLOW/REGIONS')
+
+    # Merge Data Frames by 'indicator_id'
+    zillow_data_indicators = pd.merge(zillow_data, zillow_indicators, on='indicator_id')
+    # Merge Data Frames by 'region_id'
+    zillow3 = pd.merge(zillow_data_indicators, zillow_regions, on='region_id')
+
+    # Download Merged Dataset as CSV
+    cwd = os.getcwd() + "/zillow_merged.csv"
+    zillow3.to_csv (cwd, index = False, header=True)
+
+    # Filter by year and Dowload as CSV 
+    for year in years:
+        df = zillow3[(zillow3['date'] > f"{year}-01-01") & (zillow3['date'] < f"{year}-12-31")]
+        cwd = os.getcwd()
+        cwd += f"/zillow_{year}.csv"
+        df.to_csv (cwd, index = False, header=True)
+
 
 if __name__ == '__main__':
     for year in [2014, 2015, 2016, 2017]: 
         download_and_unzip(year)
 
-
+    # Dowload Zillow data from 2014-2019 in CSV format
+    zillow_download(list(range(2014, 2019)))
